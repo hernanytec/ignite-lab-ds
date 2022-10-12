419ec66d48ecc18d2a2f8771742d0fef9c544844
hernany <hernanytec777@gmail.com>
2022-10-12 16:20:59 -0300

    feat: add TextInput component

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { EnvelopeSimple } from "phosphor-react";
+import { TextInput, TextInputRootProps } from "./TextInput";
+
+export default {
+  title: "Components/TextInput",
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <EnvelopeSimple />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail" />,
+    ],
+  },
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputRootProps> = {
+  argTypes: {
+    children: {
+      table: { disable: true },
+    },
+  },
+};
