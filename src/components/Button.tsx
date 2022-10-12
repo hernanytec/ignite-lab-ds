4be6e62b6d76901228a9f8fe2801d22f6111c9b5
hernany <hernanytec777@gmail.com>
2022-10-12 15:50:45 -0300

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  children: ReactNode;
+}
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <button className="py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white">
+      {children}
+    </button>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,7 +20,8 @@ module.exports = {
       "cyan-500": "#81D8F7",
       "cyan-300": "#9BE1FB",
 
-      black: "#000000",
+      black: "#000",
+      white: "#FFF",
     },
     extend: {
       fontFamily: {
